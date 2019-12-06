ba1bcaa595499555b375b5fdb886134701f2573e
bayapucg <bayapu.edu004@gmail.com>
2019-12-06 22:07:21 +0530

    add
    hight light sub

--- application/views/html/journal-details.php
+++ application/views/html/journal-details.php
@@ -505,11 +505,11 @@
 			</div>
 			<div class="clearfix">&nbsp;</div>
 			<div class="sidebar side-bar right-sidebar" id="article_latest">
-      <div class="widget sidebar-newsletter">
+      <div class="widget sidebar-newsletter" >
          <h3 class="side-title">Submit Manuscript</h3>
-         <div class="cp-newsletter-holder" id="pubmed">
-          <p class="card-text" style="text-align:left;border-bottom: none">
-<strong>Journal Impact Factor 1.56<sup>*</sup></strong><br> Submit manuscript at <a href="#" title="Click here"> www.grf.com/submissions/gynecology-obstetrics.html </a> or send as an e-mail attachment to the Editorial Office at <a href="" title="Click here"> obsgyne@emedicinejournals.com </a>
+         <div class="cp-newsletter-holder" id="pubmed" style="background: #4caf50;color:#fff">
+          <p class="card-text" style="text-align:left;border-bottom: none;color:#fff;">
+<strong>Journal Impact Factor 1.56<sup>*</sup></strong><br> Submit manuscript at <a style="color:#ffd658" href="#" title="Click here"> www.grf.com/submissions/gynecology-obstetrics.html </a> or send as an e-mail attachment to the Editorial Office at <a style="color:#ffd658" href="" title="Click here"> obsgyne@emedicinejournals.com </a>
 </p>
             
          </div>
